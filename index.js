e773ec8abe59b8e6fe64f7c59ac09d24489a7779
Mezahir2025 <mezahir.aliyev2025@gmail.com>
2025-08-10 22:40:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const qrcode = require("qrcode-terminal");
 
 const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ],
+    headless: true
+  }
 });
 
 client.on("qr", (qr) => {
